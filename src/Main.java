72a8247c7024b75d4767f517dd6f82a41ecd5585
Teona46 <teonika2406@gmail.com>
2024-09-01 07:58:32 +0300

    homework 01.09

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Hello Universe!");
+        byte myByte = -7;
+        short myShort = 30_000;
+        int myInt = 3_000_000;
+        long myLong = 6_000_000_000L;
+        float myFloat = 1.56f;
+        double myDouble = 3.44;
+        char myChar = 'T';
+        boolean myBoolean = true;
+
+
+        int x = 39;
+        int z = 9;
+
+        int sum = x+z;
+        int difference = x-z;
+        int product = x*z;
+        int quotient = x/z;
+        int remainder = x%z;
+
+        System.out.println("Сложение: " + sum);
+        System.out.println("Вычитание: " + difference);
+        System.out.println("Умножение: " + product);
+        System.out.println("Деление: " + quotient);
+        System.out.println("Остаток от деления: " + remainder);
+
     }
 }
